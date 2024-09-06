4663df0a313ab9430bbf456a0321da51d8b3fc91
stiven trujillo <strujillodev@gmail.com>
2024-09-06 14:52:45 -0500

    feat: test socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
+const socket = io("http://localhost:4000");
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+
+    socket.on("game over", (playerName) => {
+      console.log(playerName);
+    });
+    return () => {};
+  }, []);
+
+  const handleTakeOutBall = () => {
+    socket.emit("takeOut ball", "66da942b9c07c98f77e0ef08");
+  }
+  const handleResetGame = () => {
+    socket.emit("reset game", "66da942b9c07c98f77e0ef08");
+  }
 
   return (
     <>
@@ -18,8 +36,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleTakeOutBall}>
+          take out Ball
+        </button>
+        <button onClick={handleResetGame}>
+          reset game
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +50,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
